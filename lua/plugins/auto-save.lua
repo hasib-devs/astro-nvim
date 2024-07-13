88e3252b0e68fa54bd8075dd408178ad97517b42
Hasibur Rahman <hasib.webdev@gmail.com>
2024-07-13 21:43:36 +0600

    added auto-save and updated term mapping

---
+++ lua/plugins/auto-save.lua
@@ -0,0 +1,7 @@
+return {
+  "Pocco81/auto-save.nvim",
+  -- opts = function(_, opts)
+  --
+  --    return opts
+  --  end,
+}
---
+++ lua/plugins/mappings.lua
@@ -0,0 +1,14 @@
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    mappings = {
+      n = {
+        ["<c-\\>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
+      },
+      t = {
+        ["<c-\\>"] = { "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
+      },
+    },
+  },
+}
